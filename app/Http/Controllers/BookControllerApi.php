6d39234511de309c785aa939a5301ebd4003322c
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-09-10 14:52:06 +0100

    BookControllerApi completed

--- app/Http/Controllers/BookControllerApi.php
+++ app/Http/Controllers/BookControllerApi.php
@@ -2,17 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class BookControllerApi extends Controller
 {
     public function getAllBooks() {
-        // logic to get all books goes here
+
+        $books = Http::get("https://www.anapioficeandfire.com/api/books")->json();
+        $data = [];
+       foreach ($books as $book){
+           $myBook = new Book;
+           $myBook->name = $book['name'];
+           $myBook->authors = $book['authors'];
+           $myBook->comments_count = 12;
+           $myBook->characters = $book['characters'];
+           $date = Carbon::parse($book['released'])->format("Y-m-d");
+           $data[$date] = $myBook;
+       }
+       return $data;
     }
 
     public function getBook($id) {
-        return Http::get("https://www.anapioficeandfire.com/api/books/{$id}")->json();
+        $book = Http::get("https://www.anapioficeandfire.com/api/books/{$id}")->json();
+        $myBook = new Book;
+        $myBook->name = $book['name'];
+        $myBook->authors = $book['authors'];
+        $myBook->comments_count = 12;
+        $myBook->characters = $book['characters'];
+        return $myBook;
     }
 
     public function createBook(Request $request) {
--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class BooksController extends Controller
 {
     function show(){
-        return Http::get("https://www.anapioficeandfire.com/api")->json();
+        $myBook = new Book;
+//        return $book;
+//        $myBook->name = $book['name'];
+        echo gettype($myBook->name);
+//        var_dump(gettype($myBook) );
+//        return Http::get("https://www.anapioficeandfire.com/api")->json();
     }
 }
