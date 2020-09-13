437eaf38377843c263639f7df3960cb240cc7328
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-09-13 19:49:35 +0100

    auth removed

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 Route::get('books', [BookControllerApi::class, 'getAllBooks']);
 Route::get('books/{id}', [BookControllerApi::class, 'getBook']);
